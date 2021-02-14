c7b3a6a80e84bc571da1a9f19a5a6d2e645a8c5a
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2021-02-14 14:05:12 +0400

    Add task component and functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 import { useState } from 'react';
 
 function App() {
@@ -25,6 +26,14 @@ function App() {
     },
   ]);
 
+  //Add task
+  const addTask = (task) => {
+    // console.log(task);
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
   //Delete task
   const deleteTask = (id) => {
     // console.log(`delete ${id}`);
@@ -44,6 +53,7 @@ function App() {
   return (
     <div className='container'>
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
