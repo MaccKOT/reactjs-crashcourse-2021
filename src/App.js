71a44d9bc31d03d640dcfe48adc110ac9d96212a
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2021-02-14 12:07:16 +0400

    Expressions in jsx

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import './App.css';
-
 function App() {
+  const name = 'User';
+
   return (
-    <div className='App'>
-      <a
-        className='App-link'
-        href='https://reactjs.org'
-        target='_blank'
-        rel='noopener noreferrer'>
-        Learn React
-      </a>
-    </div>
+    <>
+      <h1>Hello, {name}</h1>
+    </>
   );
 }
 
